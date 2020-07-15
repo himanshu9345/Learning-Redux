de29711d2e5528f56f26956214c8d7c046150b69
himanshu9345 <hbp53@njit.edu>
2020-07-14 22:46:38 -0400

    presisted redux store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,24 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
 import { todos } from "./todos/reducers";
 const reducers = {
   todos,
 };
+const presistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
 const rootReducer = combineReducers(reducers);
+// presist state of application
+const persistedReducer = persistReducer(presistConfig, rootReducer);
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
-import './NewTodoForm.css';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createTodo } from "./actions";
+import "./NewTodoForm.css";
 
-const NewTodoForm = () => {
-    const [inputValue, setInputValue] = useState('');
+const NewTodoForm = ({ todos, onCreatePressed }) => {
+  const [inputValue, setInputValue] = useState("");
 
-    return (
-        <div className="new-todo-form">
-            <input
-                className="new-todo-input"
-                type="text"
-                placeholder="Type your new todo here"
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)} />
-            <button className="new-todo-button">Create Todo</button>
-        </div>
-    );
+  return (
+    <div className="new-todo-form">
+      <input
+        className="new-todo-input"
+        type="text"
+        placeholder="Type your new todo here"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          const isDuplicateText = todos.some((todo) => todo === inputValue);
+          if (!isDuplicateText) {
+            onCreatePressed(inputValue);
+            setInputValue("");
+          }
+        }}
+        className="new-todo-button"
+      >
+        Create Todo
+      </button>
+    </div>
+  );
 };
+// state => redux state,
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+// dispatch
+const mapDispatchToProps = (dispatch) => ({
+  onCreatePressed: (text) => dispatch(createTodo(text)),
+});
 
-export default NewTodoForm;
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
