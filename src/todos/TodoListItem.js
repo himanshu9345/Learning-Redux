9a7ded6c37f241bb058c0fee49becabaa5aabd01
himanshu9345 <hbp53@njit.edu>
2020-07-28 17:21:48 -0400

    passing props to styled components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -6,14 +6,22 @@ const TodoItemContainer = styled.div`
   background: #fff;
   border-radius: 8px;
   margin-top: 8px;
+  border-bottom: ${(props) =>
+    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+      ? "none"
+      : "2px solid red"};
   padding: 16px;
   position: relative;
   box-shadow: 0 4px 8px grey;
 `;
 
 const TodoListItem = ({ todo, onRemovedPressed, markTaskCompleted }) => (
-  <TodoItemContainer>
+  <TodoItemContainer createdAt={todo.createdAt}>
     <h3>{todo.text}</h3>
+    <p>
+      Created at: &nbsp;
+      {new Date(todo.createdAt).toLocaleDateString()}
+    </p>
     <div className="buttons-container">
       {todo.isCompleted ? null : (
         <button
